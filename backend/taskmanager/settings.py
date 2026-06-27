7cd410f49e9465538210c108bc3abb15b7281731
Mohammed Aqueel <mohammed.aqueel36@gmail.com>
2026-06-27 05:34:02 +0530

    Updated setting.py for vercel

--- backend/taskmanager/settings.py
+++ backend/taskmanager/settings.py
@@ -79,13 +79,16 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5176',
 ]
-CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_CREDENTIALS = False
+
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
